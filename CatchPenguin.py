38d8048104f8c5e61a69a2d9043a78375212505c
Esben922 <TR67NH@student.aau.dk>
2025-03-19 18:52:40 +0100

    Cleanup

--- CatchPenguin.py
+++ CatchPenguin.py
@@ -3,8 +3,8 @@ import joblib
 import requests
 
 #Load our model
-clf = joblib.load("penguin_classifier.pkl")
-label_encoder = joblib.load("label_encoder.pkl")
+clf = joblib.load("models/penguin_classifier.pkl")
+label_encoder = joblib.load("models/label_encoder.pkl")
 
 #API endpoint where we fetch data
 url = "http://130.225.39.127:8000/new_penguin/"
@@ -24,7 +24,7 @@ species_encoded = clf.predict(features)[0]
 species = label_encoder.inverse_transform([species_encoded])[0]
 
 #I connect to our database for the predicted penguins and store the data
-conn = sqlite3.connect("PenguinAlert.sqlite")
+conn = sqlite3.connect("data/PenguinAlert.sqlite")
 cursor = conn.cursor()
 cursor.execute('''
     INSERT INTO PenguinPropertiesPredicted (bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, species)
@@ -32,3 +32,5 @@ cursor.execute('''
 ''', (data["bill_length_mm"], data["bill_depth_mm"], data["flipper_length_mm"], data["body_mass_g"], species))
 conn.commit()
 conn.close()
+
+print(f"Predicted species: {species}")
